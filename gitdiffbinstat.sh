435096a9574b20097309c6c943c7f086d8a712e8
Matthias Krüger <matthias.krueger@famsik.de>
2014-03-07 19:38:02 +0100

    bump copyright to 2014

--- gitdiffbinstat.sh
+++ gitdiffbinstat.sh
@@ -2,7 +2,7 @@
 
 
 #    gitdiffbinstat - gets a git diff --shortstat-like output for changed binary files
-#    Copyright (C) 2013  Matthias Krüger
+#    Copyright (C) 2014  Matthias Krüger
 
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
