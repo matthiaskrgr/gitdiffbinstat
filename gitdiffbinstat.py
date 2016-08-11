b9d23b6697a36d1d246c40cbd6e40e9ec2f3c36d
Matthias Krüger <matthias.krueger@famsik.de>
2016-08-11 11:00:06 +0200

    begin porting to python

---
+++ gitdiffbinstat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#    gitdiffbinstat - gets a git diff --shortstat-like output for changed binary files
+#    Copyright (C) 2016  Matthias Krüger
+
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 1, or (at your option)
+#    any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
+
+
+import sys
+if (sys.version_info.major != 3): # no python 3
+	print("Python 3 or higher is required.")
+	sys.exit(1)
+
