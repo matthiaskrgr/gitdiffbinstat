a6acf1f9fdbfb918de025ddf6180bbcea570b18f
Matthias Krüger <matthias.krueger@famsik.de>
2017-08-05 14:55:38 +0200

    enable clippy for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+// execute with      cargo run --features "clippy"
+
+
+fn help() {
+    println!("Usage: 'gitdiffbinstat [<commit/branch/tag/HEAD>]'");
+    println!("Make sure to be inside a git repository!");
 }
+
+
+fn main() {
+    println!("Hello, world!\n");
+    help();
+}
+
+
